a4e51a1e70e8189c4b7ad63dbb622ef9d47db7d5
Alex Gnip <gnip.alex.ag@yandex.ru>
2014-09-12 00:55:35 +0300

    some changes to 3.2 ui

--- task3/src/com/epam/hnyp/task3/subtask2/ConsoleUI.java
+++ task3/src/com/epam/hnyp/task3/subtask2/ConsoleUI.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.util.Scanner;
 
 import com.epam.hnyp.task3.subtask2.condition.BaseFileCondition;
+import com.epam.hnyp.task3.subtask2.condition.ExtensionFileCondition;
+import com.epam.hnyp.task3.subtask2.condition.FileSizeCondition;
+import com.epam.hnyp.task3.subtask2.condition.NameFileCondition;
 
 public class ConsoleUI {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Please specify search folder or pass enter to cancel: ");
+		System.out
+				.print("Please specify search folder or pass enter to cancel: ");
 		String line = sc.nextLine();
 		if (line.isEmpty()) {
 			return;
@@ -22,27 +26,87 @@ public class ConsoleUI {
 			System.out.println("--Specified path is not directory--");
 			return;
 		}
-		
+
 		System.out.println("Search in dir : " + startDir.getAbsolutePath());
 		BaseFileCondition filter = null;
-		
-		while (true) {
-			System.out.print("Use name of file ? (1/0)");
-			line = sc.nextLine();
-			if (line.isEmpty() || line.charAt(0) == '0') {
+
+		System.out.print("Use name filter ? (y - yes)");
+		line = sc.nextLine();
+		if (!line.isEmpty() && line.charAt(0) == 'y') {
+			while (true) {
+				System.out.print("Enter name of file : ");
+				line = sc.nextLine();
+				if (line.isEmpty()) {
+					continue;
+				}
 				break;
 			}
-			if (line.charAt(0) != '1') {
-				continue;
+			filter = new NameFileCondition(line, null);
+		}
+
+		System.out.print("Use extension filter ? (y - yes)");
+		line = sc.nextLine();
+		if (!line.isEmpty() && line.charAt(0) == 'y') {
+			while (true) {
+				System.out.print("Enter extension of file : ");
+				line = sc.nextLine();
+				if (line.isEmpty()) {
+					continue;
+				}
+				break;
 			}
-			System.out.print("Enter name of file : ");
-			line = sc.nextLine();
-			break;
+			filter = new ExtensionFileCondition(line, filter);
+		}
+
+		System.out.print("Use size filter ? (y - yes)");
+		line = sc.nextLine();
+		if (!line.isEmpty() && line.charAt(0) == 'y') {
+			long minSz = 0, maxSz = 0;
+			while (true) {
+				while (true) {
+					System.out.print("Enter min size of file : ");
+					line = sc.nextLine();
+					if (line.isEmpty()) {
+						continue;
+					}
+					try {
+						minSz = Long.parseLong(line);
+					} catch (NumberFormatException e) {
+						System.out.println("##wrong format##");
+						continue;
+					}
+					break;
+				}
+				while (true) {
+					System.out.print("Enter max size of file : ");
+					line = sc.nextLine();
+					if (line.isEmpty()) {
+						continue;
+					}
+					try {
+						maxSz = Long.parseLong(line);
+					} catch (NumberFormatException e) {
+						System.out.println("##wrong format##");
+						continue;
+					}
+					break;
+				}
+				if (minSz < 0 || maxSz < minSz) {
+					System.out.println("##Sizes must be min >= 0 and min <= max##");
+					continue;
+				}
+				break;
+			}
+			filter = new FileSizeCondition(minSz, maxSz, filter);
 		}
 		
+		System.out.print("Use size filter ? (y - yes)");
+		line = sc.nextLine();
+		if (!line.isEmpty() && line.charAt(0) == 'y') {
+			
+		}
 		
-		
-		
-		
+
 	}
+
 }
