a2edaf77ade1b47851d2363cc9744935ac3f542f
Alex Gnip <gnip.alex.ag@yandex.ru>
2014-10-12 22:54:23 +0300

    started 8

---
+++ task8/register_validator.js
@@ -0,0 +1,14 @@
+function resetError(container) {
+	container.className = '';
+	if (container.lastChild.)
+		http://learn.javascript.ru/play/tutorial/form/validate.html
+}
+
+function showError() {
+	
+}
+
+function validate(form) {
+	var elems = form.elements;
+	
+}
